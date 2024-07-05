ac410f10959e1e8419e0afd215096878c053a8c6
Jared Bellamy <57675302+Bellamyjared@users.noreply.github.com>
2024-07-05 09:52:48 -0600

    Update tests/assist.handler.spec.ts
    Changed expected content types to match test.
    
    Co-authored-by: Joakim Sørensen <joasoe@gmail.com>

--- tests/assist.handler.spec.ts
+++ tests/assist.handler.spec.ts
@@ -95,7 +95,7 @@ describe("Assist handler", function () {
     const result: Record<string, string> = await response.json();
     expect(response.status).toBe(201);
     expect(result.message).toStrictEqual("success");
-    expect(result.key.endsWith(".webm")).toBeTruthy();
+    expect(result.key.endsWith(".ogg")).toBeTruthy();
     expect(MockEvent.env.WAKEWORD_TRAINING_BUCKET.put).toHaveBeenCalledTimes(1);
   });
 
